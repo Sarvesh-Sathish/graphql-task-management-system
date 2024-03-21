8e594624597e20fb80be2f408b92be99c2852e5e
Sarvesh-Sathish <sarveshsathish@gatech.edu>
2024-03-21 12:05:08 -0500

    init project

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+from .schema import schema
+from flask_graphql import GraphQLView
+
 
 app = Flask(__name__)
 
@@ -9,4 +12,13 @@ def hello_world():
     return render_template('index.html', message = message)
 
 if __name__ == "__main__":
+    app.add_url_rule(
+        '/graphql',
+        view_func=GraphQLView.as_view(
+            'graphql',
+            schema=schema,
+            graphiql=True  # for having the GraphiQL interface
+        )
+    )
     app.run(debug=True)
+    
