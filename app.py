a05ec724000c75261c15eb1381bf059ef3cd963c
Sarvesh-Sathish <sarveshsathish@gatech.edu>
2024-03-21 15:59:47 -0500

    fixed user dup error

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from flask import render_template
 from query import Query
+from mutation import CreateUser
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     q = Query()
-    message = str(q.resolve_users(None))
+    newEmail = 'b@b.com'
+    newPass = 'ppp'
+    message = ''
     
+    if q.resolve_user(None, newEmail) is None:
+        CreateUser.mutate(None, None, newEmail, newPass)
+    message = str(q.resolve_users(None))
     return render_template('index.html', message = message)
 
 if __name__ == "__main__":
--- schema.py
+++ schema.py
@@ -3,4 +3,4 @@ from query import Query
 from mutation import Mutation
 
 schema = Schema(query=Query, mutation=Mutation)
-print(schema)
+# print(schema)
